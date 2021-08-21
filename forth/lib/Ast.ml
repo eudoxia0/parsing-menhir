6833d4c13d444a6a8a78f7fd76040518fc9dbedd
Fernando Borretti <fernando@borretti.me>
2021-08-21 00:18:15 +0000

    Better printing

--- forth/lib/Ast.ml
+++ forth/lib/Ast.ml
@@ -7,11 +7,11 @@ type program = Program of atom list
 
 let dump_atom = function
   | Int i ->
-     "Int(" ^ string_of_int i ^ ")"
+     "Int " ^ string_of_int i
   | Float f ->
-     "Float(" ^ string_of_float f ^ ")"
+     "Float " ^ string_of_float f
   | Word s ->
-     "Word(" ^ s ^ ")"
+     "Word \"" ^ s ^ "\""
 
 let dump_program (Program atoms) =
-  String.concat " " (List.map dump_atom atoms)
+  "[" ^ (String.concat ", " (List.map dump_atom atoms)) ^ "]"
